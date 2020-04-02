c2f29de54c477811208641ee5419ee620cb666f6
Scott Stokes <sstokes99@gmail.com>
2020-04-01 21:48:03 -0500

    added basic accountList component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AccountListComponent } from './account-list/account-list.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "", component: AccountListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
